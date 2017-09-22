60cd721f319e40b71dbd35c64b87aeacf00303a8
Cristian Andres Olivares Canales <cristian.a.olivares.c@hotmail.com>
2017-09-21 22:19:50 -0300

    dasda

--- OrionEscritorio/OrionEscritorio/TDepartamento.cs
+++ OrionEscritorio/OrionEscritorio/TDepartamento.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Oracle.ManagedDataAccess.Client;
+using Oracle.DataAccess.Client;
 
 namespace OrionEscritorio
 {
@@ -19,23 +19,5 @@ namespace OrionEscritorio
             return resp;
         }
 
-        
-        /*public static List<Cliente> listarClientes()
-        {
-            List<Cliente> lista = new List<Cliente>();
-            MySqlConnection conexion = Conexion.abrirConexion();//Singleton
-            MySqlCommand orden = new MySqlCommand(string.Format("SELECT * FROM CLIENTE"), conexion);
-            MySqlDataReader lector = orden.ExecuteReader();
-            while (lector.Read())
-            {
-                Cliente cliente = new Cliente();
-                cliente.rut = lector.GetString(0);
-                cliente.nombre = lector.GetString(1);
-                cliente.apel = lector.GetString(2);
-                cliente.fono = lector.GetString(3);
-                lista.Add(cliente);
-            }
-            return lista;
-        }*/
     }
 }
