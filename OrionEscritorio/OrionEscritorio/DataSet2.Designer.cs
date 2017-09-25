1c8fcb3113863ffe1b5d09d2190bea823ea58fad
esteban moraga <dragon200_144@hotmail.com>
2017-09-24 21:57:38 -0300

    cambios pequeños en form tipos

--- OrionEscritorio/OrionEscritorio/DataSet2.Designer.cs
+++ OrionEscritorio/OrionEscritorio/DataSet2.Designer.cs
@@ -14870,12 +14870,17 @@ namespace OrionEscritorio.DataSet2TableAdapters {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::Oracle.ManagedDataAccess.Client.OracleCommand[1];
+            this._commandCollection = new global::Oracle.ManagedDataAccess.Client.OracleCommand[2];
             this._commandCollection[0] = new global::Oracle.ManagedDataAccess.Client.OracleCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT ID, NOMBRE, DESCRIPCION, PERMISO_IDPERMISO, ESTADO_IDESTADO FROM MATIAS.TI" +
                 "PO";
             this._commandCollection[0].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[1] = new global::Oracle.ManagedDataAccess.Client.OracleCommand();
+            this._commandCollection[1].Connection = this.Connection;
+            this._commandCollection[1].CommandText = "SELECT ID, NOMBRE, DESCRIPCION, PERMISO_IDPERMISO, ESTADO_IDESTADO FROM MATIAS.TI" +
+                "PO";
+            this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -14900,6 +14905,19 @@ namespace OrionEscritorio.DataSet2TableAdapters {
             DataSet2.TIPODataTable dataTable = new DataSet2.TIPODataTable();
             this.Adapter.Fill(dataTable);
             return dataTable;
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Fill, false)]
+        public virtual int FillBy(DataSet2.TIPODataTable dataTable) {
+            this.Adapter.SelectCommand = this.CommandCollection[1];
+            if ((this.ClearBeforeFill == true)) {
+                dataTable.Clear();
+            }
+            int returnValue = this.Adapter.Fill(dataTable);
+            return returnValue;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
--- OrionEscritorio/OrionEscritorio/Tipo.cs
+++ OrionEscritorio/OrionEscritorio/Tipo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,20 @@ namespace OrionEscritorio
         public int idTipo { get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
+
+
+        public int ingresarTipo(Test test)
+        {
+            int resp = 0;
+            OracleConnection conexion = Conexion.abrirConexion();
+            OracleCommand orden = new OracleCommand(string.Format("INSERT INTO Tipo(ID,NOMBRE,DESCRIPCION,PERMISO_IDPERMISO,ESTADO_IDESTADO) VALUES('')", test.datoqlo), conexion);
+            resp = orden.ExecuteNonQuery();
+            conexion.Close();
+            return resp;
+        }
+
+
     }
+
+
 }
