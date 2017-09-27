32506a708c8a3654e74239c724a5aa5b0255e51e
esteban moraga <dragon200_144@hotmail.com>
2017-09-27 02:09:17 -0300

    cambios Ingresar

--- OrionEscritorio/OrionEscritorio/FrmUsuarios.cs
+++ OrionEscritorio/OrionEscritorio/FrmUsuarios.cs
@@ -22,6 +22,21 @@ namespace OrionEscritorio
 
         }
 
-      
+        private void btnCUsuario_Click(object sender, EventArgs e)
+        {
+
+            String Usuario = txtUsuario.Text.ToString();
+            String Password = txtPassword.Text.ToString();
+            String IdFunc = txtFuncionario.Text.ToString();
+            int perfil = Convert.ToInt32(cbxPerfil.ToString());
+            TUsuario tusuario = new TUsuario();
+            Usuario usuario = new Usuario();
+            usuario.nombre = Usuario;
+            usuario.clave = Password;
+            usuario.rut_funcionario = IdFunc;
+            usuario.perfil_id = perfil;
+            tusuario.ingresarUsuario(usuario);
+
+        }
     }
 }
